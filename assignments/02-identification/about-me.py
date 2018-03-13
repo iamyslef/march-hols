cc181c68dabb7edb46e21464f17b83cda2cccf2f
iam <iamyslef@gmail.com>
2018-03-13 16:34:25 +0800

    CSC March Holidays Assignment
    :D

--- assignments/02-identification/about-me.py
+++ assignments/02-identification/about-me.py
@@ -8,16 +8,16 @@ print("This gets printed into console")
 
 # Now we need to create some variables.
 # Create a variable called "name" and assign it your own name as a string.
-
+name = "iamyslef"
 
 # Create a variable called "age" and assign it your own age as an integer.
-
+age = 9
 
 # Create a variable called "coolness" and give it a float.
-
+coolness = 9000.1
 
 # Create a variable called "likes_arnold" and give it a boolean.
-
+likes_arnold = False
 
 
 
--- assignments/02-identification/get-details.py
+++ assignments/02-identification/get-details.py
@@ -3,23 +3,30 @@ name = input("What is your name? ")
 # If length of name is greater than 20,
 # print something
 
+if len(name) > 20:
+    print("Why can't your name be short and sweet, like 'pyra'?")
 
-
-age = input("What is your age? ")
+age = int(input("What is your age? "))
 
 # If age is less than 10, print "Smol"
 # ELse if age is between 10 and 20, print "Big boi"
 # Else, print "Big big boi"
 
+if age < 10:
+    print("Congrats! You're younger than pyra!")
+elif age > 10 and age < 20:
+    print("Congrats! You're close to being pyrya's age!")
+else:
+    print("Congrats! You're older than pyra!")
 
-
-coolness = input("Rate your coolness out of 100.0")
+coolness = float(input("Rate your coolness out of 100.0"))
 
 # If coolness is more than 100.0, just print some error
 
-
+if coolness > 100.0:
+    print("Lies. You can't be cooler than pyra!")
 
 # Now print a string like
 # My name is Arnold Tan, I am 69 and I'm Really Cool
 
-
+print("My name is pyra, I am a weeb and I love anime.")
